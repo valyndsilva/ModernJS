3a5d58ead2365fa711d748eb76285abfd857a590
valyndsilva <valyndsilva@gmail.com>
2021-06-06 13:03:59 +0100

    ES6 Classes

--- JS_Sandbox/Object_Oriented JS_ES5_ES2015/app.js
+++ JS_Sandbox/Object_Oriented JS_ES5_ES2015/app.js
@@ -188,30 +188,65 @@
 //   console.log(customer1.greeting());
 
 
-// V.Using Object.create()
-// Create prototypes in parent objects and have different properties with different prototype methods or functions.
+// // V.Using Object.create()
+// // Create prototypes in parent objects and have different properties with different prototype methods or functions.
 
-const personPrototypes = {
-    greeting: function() {
+// const personPrototypes = {
+//     greeting: function() {
+//       return `Hello there ${this.firstName} ${this.lastName}`;
+//     },
+//     getsMarried: function(newLastName) { //prototype method getsMarried
+//       this.lastName = newLastName;
+//     }
+//   }
+//   const mary = Object.create(personPrototypes); // create an object mary and pass the personPrototypes
+//   mary.firstName = 'Mary';
+//   mary.lastName = 'Williams';
+//   mary.age = 30;
+//   console.log(mary.greeting());
+
+//   mary.getsMarried('Thompson');
+//   console.log(mary.greeting());
+  
+//   const valyn = Object.create(personPrototypes, {
+//     firstName: {value: 'Valyn'},
+//     lastName: {value: 'Maria'},
+//     age: {value: 29}
+//   });
+//   console.log(valyn);
+//   console.log(valyn.greeting());
+
+// VI. ES6 Classes:
+class Person {
+    constructor(firstName, lastName, dob) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.birthday = new Date(dob);
+    }
+  
+    greeting() { //any method added to the class gets added to the prototype
       return `Hello there ${this.firstName} ${this.lastName}`;
-    },
-    getsMarried: function(newLastName) { //prototype method getsMarried
+    }
+  
+    calculateAge() {
+      const diff = Date.now() - this.birthday.getTime();
+      const ageDate = new Date(diff);
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+  
+    getsMarried(newLastName) {
       this.lastName = newLastName;
     }
-  }
-  const mary = Object.create(personPrototypes); // create an object mary and pass the personPrototypes
-  mary.firstName = 'Mary';
-  mary.lastName = 'Williams';
-  mary.age = 30;
-  console.log(mary.greeting());
-
-  mary.getsMarried('Thompson');
-  console.log(mary.greeting());
   
-  const valyn = Object.create(personPrototypes, {
-    firstName: {value: 'Valyn'},
-    lastName: {value: 'Maria'},
-    age: {value: 29}
-  });
-  console.log(valyn);
-  console.log(valyn.greeting());
+    static addNumbers(x, y) {
+      return x + y;
+    }
+  }
+  
+  const mary = new Person('Mary', 'Williams', '11-13-1980'); //instantiate (ex: create object from class)
+  
+  console.log(mary);
+  console.log(mary.greeting());
+  console.log(mary.calculateAge());
+  console.log(mary.getsMarried('Thompson'));
+  console.log(Person.addNumbers(1,2));
