51b232286cefc73b05ceffd32b923ab93a420a7c
valyndsilva <valyndsilva@gmail.com>
2021-06-07 08:34:27 +0100

    Fetch API Error Handling

--- JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/5.Fetch_API/app.js
+++ JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/5.Fetch_API/app.js
@@ -60,3 +60,18 @@ function getExternal() {
       console.log(err);
     });
 }
+
+// Error Handling With Fetch
+// Error handling with fetch is a bit different than with something like Axios or jQuery. 
+// If there is an http error, it will not fire off .catch automatically. 
+// You have to check the response and throw an error yourself.
+function handleErrors(res) {
+    if (!res.ok) throw new Error(res.error);
+    return res;
+  }
+   
+  fetch('https://devcamper.io/api/v1/bootcamps/34343')
+    .then(res => res.json())
+    .then(handleErrors)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
