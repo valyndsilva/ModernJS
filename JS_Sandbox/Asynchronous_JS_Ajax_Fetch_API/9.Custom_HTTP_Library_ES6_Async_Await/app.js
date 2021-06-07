afab8afe9d2c6b3e377ba76832ff384ce1f61834
valyndsilva <valyndsilva@gmail.com>
2021-06-07 16:00:31 +0100

    EasyHTTP Library using Fetch API with Async and Await

---
+++ JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/9.Custom_HTTP_Library_ES6_Async_Await/app.js
@@ -0,0 +1,32 @@
+const http = new easyHTTP;
+
+// // const users = http.get('https://jsonplaceholder.typicode.com/users');
+// // console.log(users); //returns a promise which is not what we want. We need the data from the promise (response).
+
+// // Get Users (GET)
+// http.get('https://jsonplaceholder.typicode.com/users')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+
+// Create User Data
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
+
+// // Create User (POST)
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+// // Update User (PUT)
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+// Delete User (DELETE)
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
