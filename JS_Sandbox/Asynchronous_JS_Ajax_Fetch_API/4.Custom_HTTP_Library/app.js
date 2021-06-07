9269b604dfd3fe37cdbc5cad3b587590f598e4d7
valyndsilva <valyndsilva@gmail.com>
2021-06-07 07:46:49 +0100

    EasyHTTP PUT Request

--- JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/4.Custom_HTTP_Library/app.js
+++ JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/4.Custom_HTTP_Library/app.js
@@ -1,6 +1,6 @@
 const http = new easyHTTP;
 
-// // Get Posts
+// // Get Posts (GET)
 // http.get('https://jsonplaceholder.typicode.com/posts', function(err, responsePosts) {
 //   if(err) {
 //     console.log(err);
@@ -9,7 +9,7 @@ const http = new easyHTTP;
 //   }
 // });
 
-// // Get Single Post
+// // Get Single Post (GET)
 // http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, responsePosts) {
 //   if(err) {
 //     console.log(err);
@@ -24,23 +24,23 @@ const data = {
     body: 'This is a custom post'
   };
   
-  // Create Post
-  // http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, responsePosts) {
-  //   if(err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(responsePosts);
-  //   }
-  // });
+//   // Create Post (POST)
+//   http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, responsePosts) {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       console.log(responsePosts);
+//     }
+//   });
   
-  // Update Post
-  // http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, responsePosts) {
-  //   if(err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(responsePosts);
-  //   }
-  // });
+  // Update Post (PUT)
+  http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, responsePosts) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(responsePosts);
+    }
+  });
   
   // Delete Post
   http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, responsePosts) {
