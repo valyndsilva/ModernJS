5e1c5905f6eb2d9f8c4588f7b74483b407dfbb9b
valyndsilva <valyndsilva@gmail.com>
2021-06-07 07:40:58 +0100

    EasyHTTP POST Request

--- JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/4.Custom_HTTP_Library/app.js
+++ JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/4.Custom_HTTP_Library/app.js
@@ -1,10 +1,52 @@
 const http = new easyHTTP;
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(posts);
-  }
-});
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, responsePosts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(responsePosts);
+//   }
+// });
+
+// // Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, responsePosts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(responsePosts);
+//   }
+// });
+
+// Create Data
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+  };
+  
+  // Create Post
+  // http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, responsePosts) {
+  //   if(err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log(responsePosts);
+  //   }
+  // });
+  
+  // Update Post
+  // http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, responsePosts) {
+  //   if(err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log(responsePosts);
+  //   }
+  // });
+  
+  // Delete Post
+  http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, responsePosts) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(responsePosts);
+    }
+  });
--- JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/4.Custom_HTTP_Library/easyHttp.js
+++ JS_Sandbox/Asynchronous_JS_Ajax_Fetch_API/4.Custom_HTTP_Library/easyHttp.js
@@ -12,7 +12,7 @@ function easyHTTP() {
           //'this' here is undefined within a function in ES5. Use let self = this;
           //'this'' does work within the function if you use ES6 arrow functions as it uses a lexical 'this'
         //   console.log(this.http.responseText); 
-        callback(null, self.http.responseText);
+        callback(null, self.http.responseText); // cb(error, responsePosts)
       } else {
         callback('Error: ' + self.http.status);
       }
@@ -21,10 +21,48 @@ function easyHTTP() {
     this.http.send();
   }
   
-  // Make an HTTP POST Request
+
+// Make an HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+  
+    let self = this;
+    this.http.onload = function() {
+      callback(null, self.http.responseText);
+    }
+  
+    this.http.send(JSON.stringify(data));
+  }
+  
   
   // Make an HTTP PUT Request
+  easyHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+  
+    let self = this;
+    this.http.onload = function() {
+      callback(null, self.http.responseText);
+    }
+  
+    this.http.send(JSON.stringify(data));
+  }
   
   // Make an HTTP DELETE Request
+  easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+  
+    let self = this;
+    this.http.onload = function() {
+      if(self.http.status === 200) {
+        callback(null, 'Post Deleted');
+      } else {
+        callback('Error: ' + self.http.status);
+      }
+    }
+  
+    this.http.send();
+  }
   
   
