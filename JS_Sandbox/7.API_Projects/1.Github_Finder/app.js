fd87083ff8798c92d624cf92fe689b9d242df3c0
valyndsilva <valyndsilva@gmail.com>
2021-06-08 04:19:15 +0100

    Github USer Finder - Fetching Profile Data

--- JS_Sandbox/7.API_Projects/1.Github_Finder/app.js
+++ JS_Sandbox/7.API_Projects/1.Github_Finder/app.js
@@ -1,2 +1,30 @@
-// Client Secret for Github OAuth App: a9f550c58cdbfed1afd491ad2ef5fcce44132d56
-// Client ID: 2e23f24a352e3d4ba0b0
+
+// Init Github class
+const github = new Github;
+
+// UI Vars: Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') { //default message from github
+        // Show alert
+        
+
+      } else {
+        // Show profile
+        console.log(data);
+      }
+    })
+  } else {
+    // Clear profile as you delete a character from the username
+    
+  }
+}); 
---
+++ JS_Sandbox/7.API_Projects/1.Github_Finder/github.js
@@ -0,0 +1,19 @@
+// Client Secret for Github OAuth App: a9f550c58cdbfed1afd491ad2ef5fcce44132d56
+// Client ID: 2e23f24a352e3d4ba0b0
+
+class Github {
+    constructor() {
+      this.client_id = '2e23f24a352e3d4ba0b0';
+      this.client_secret = 'a9f550c58cdbfed1afd491ad2ef5fcce44132d56';
+    }
+  
+    async getUser(user) {
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const profile = await profileResponse.json();
+  
+      return {
+        profile
+      }
+    }
+  }
