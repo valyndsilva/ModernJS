e6fab5319865e2ad4d56f61ae6e8ea260da624c2
valyndsilva <valyndsilva@gmail.com>
2021-06-09 14:11:24 +0100

    Regular Expressions- Character Sets & Quantifiers

--- JS_Sandbox/8.Error_Handling_Regular_Expressions/app.js
+++ JS_Sandbox/8.Error_Handling_Regular_Expressions/app.js
@@ -85,25 +85,59 @@
 // const newStr = str.replace(re, 'Hi');
 // console.log(newStr); // returns 'Hi There'
 
-// // III.Regular expressions: Metacharacter Symbols
+// III.Regular expressions: Metacharacter Symbols
 let re;
 // Literal Characters
 re = /hello/;
-re = /hello/i; // i  - case insensitive
+re = /hello/i;
 
 // Metacharacter Symbols
 re = /^h/i;           // Must start with
-re = / world$/i;     // Must end with 'world'
+re = / world$/i;     // Must ends with
 re = /^hello$/i;     // Must begin and end with
 re = /h.llo/i;      // Matches any ONE character
 re = /h*llo/i;      // Matches any character 0 or more times
-re = /gre?a?y/i;    // Optional character for gray or grey using ? as optional
+re = /gre?a?y/i;    // Optional character
 re = /gre?a?y\?/i;    // Escape character 
 
 
-// String to match
-const str = 'Gray?'; //Ex: Grey, Gray, Gry match /gre?a?y/i. Griy doesn't.
+// Brackets [] - Character Sets
+re = /gr[ae]y/i;      // Must be an a or e
+re = /[GF]ray/i;      // Must be a G or F
+re = /[^GF]ray/i;      // Match anything except a G or F
+re = /[A-Z]ray/;      // Match any uppercase letter
+re = /[a-z]ray/;      // Match any lowercase letter
+re = /[A-Za-z]ray/;   // Match any  letter
+re = /[0-9][0-9]ray/;      // Match any digit
 
+// Braces {} - Quantifiers
+re = /Hel{2}o/i;      // Must occur exactly {m} amount of times - character before {m}
+re = /Hel{2,4}o/i;      // Must occur exactly {m} amount of times
+re = /Hel{2,}o/i;      // Must occur at least {m} times
+
+// Paretheses () - Grouping
+re = /^([0-9]x){3}$/
+
+// // String to match
+// const str = '3x3x3x';
+
+// Shorthand Character Classes
+re = /\w/;    // Word character \w is alphanumeric(any letter or number or _)
+re = /\w+/;    // + = one or more word character
+re = /\W/;    // Non-Word character \W (anything but a letter or number or _)
+re = /\d/;    // Match any digit \d
+re = /\d+/;    // Match any digit 0 or more times
+re = /\D/;      // Match any Non-digit \D
+re = /\s/;      // Match whitespace char \s
+re = /\S/;      // Match non-whitespace char \S
+re = /Hell\b/i; // Word boundary
+
+// Assertions
+re = /x(?=y)/;  // Match x only if followed by y
+re = /x(?!y)/;  // Match x only if NOT followed by y
+
+// String to match
+const str = 'dkjekdxydjekdj';
 
 // Log Results
 const result = re.exec(str);
