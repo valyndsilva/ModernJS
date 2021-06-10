ea2d31ef39b351325c6da32faedae3940729463b
valyndsilva <valyndsilva@gmail.com>
2021-06-10 06:55:54 +0100

    ES6 Sets

--- JS_Sandbox/9.Additional_ES2015+_Features/app.js
+++ JS_Sandbox/9.Additional_ES2015+_Features/app.js
@@ -144,58 +144,117 @@
 // sayHello();
 
 
-// // V. ES6 MAPS = key-value pairs - can use ANY type as a key or value
+// // // V. ES6 MAPS = key-value pairs - can use ANY type as a key or value
 
-const map1 = new Map();
+// const map1 = new Map();
 
-// Set Keys
-const key1 = 'some string', 
-      key2 = {},  //empty object
-      key3 = function() {}; // empty function
+// // Set Keys
+// const key1 = 'some string', 
+//       key2 = {},  //empty object
+//       key3 = function() {}; // empty function
 
-// Set map values by key
-map1.set(key1, 'Value of key1');
-map1.set(key2, 'Value of key2');
-map1.set(key3, 'Value of key3');
+// // Set map values by key
+// map1.set(key1, 'Value of key1');
+// map1.set(key2, 'Value of key2');
+// map1.set(key3, 'Value of key3');
 
-// Get values by key
-// console.log(map1.get(key1), map1.get(key2), map1.get(key3));
+// // Get values by key
+// // console.log(map1.get(key1), map1.get(key2), map1.get(key3));
 
-// Count values
-// console.log(map1.size);
+// // Count values
+// // console.log(map1.size); // returns 3
 
-// ITERATING MAPS
+// // Va. ITERATING MAPS
 
-// Loop using for...of to get keys and values
-// for(let [key, value] of map1) {
-//   console.log(`${key} = ${value}`);
+// // Loop using for...of to get keys and values
+// // for(let [key, value] of map1) {
+// //   console.log(`${key} = ${value}`);
+// // }
+
+// // Iterate keys only
+// // for(let key of map1.keys()) {
+// //   console.log(key);
+// // }
+
+// // Iterate values only
+// // for(let value of map1.values()) {
+// //   console.log(value);
+// // }
+
+// // Loop with forEach
+// // map1.forEach(function(value, key){
+// //   console.log(`${key} = ${value}`);
+// // });
+
+// // Vb.CONVERT MAPS TO ARRAYS
+
+// // Create an array of the key value pairs
+// const keyValArr = Array.from(map1);
+// console.log(keyValArr);
+
+// // Create an array of the values
+// const valArr = Array.from(map1.values());
+// console.log(valArr);
+
+// // Create an array of the keys
+// const keyArr = Array.from(map1.keys());
+// console.log(keyArr);
+
+// // VI.ES6 Sets
+// SETS - Store unique values of any type
+
+const set1 = new Set();
+
+// VIa.Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({name: 'John'});
+set1.add(true);
+set1.add(100);
+// console.log(set1);
+
+// const set2 = new Set([1, true, 'string']);
+// console.log(set2);
+
+
+// VIb.Get count
+// console.log(set1.size);
+
+// VIc.Check for values
+// console.log(set1.has(100)); //true
+// console.log(set1.has(50 + 50)); //true
+// console.log(set1.has({name: 'John'})); //false
+// console.log({name: 'John'} === {name: 'John'});  //false because it's not a primitive value type but a reference object
+// console.log('John' === 'John'); // true since it's a primitive value type of string
+
+// VId.Delete from set
+// set1.delete(100);
+// console.log(set1);
+
+// VIe.ITERATING THROUGH SETS
+
+// For..of 
+// for(let item of set1) {
+//   console.log(item);
 // }
 
-// Iterate keys only
-// for(let key of map1.keys()) {
-//   console.log(key);
+// for(let item of set1.keys()) {
+//   console.log(item);
 // }
 
-// Iterate values only
-// for(let value of map1.values()) {
+// for(let item of set1.values()) {
+//   console.log(item);
+// }
+
+// for(let item of set1.entries()) {
+//   console.log(item);
+// }
+
+// ForEach Loop
+// set1.forEach((value) => {
 //   console.log(value);
-// }
-
-// Loop with forEach
-// map1.forEach(function(value, key){
-//   console.log(`${key} = ${value}`);
 // });
 
-// CONVERT TO ARRAYS
-
-// Create an array of the key value pairs
-const keyValArr = Array.from(map1);
-console.log(keyValArr);
-
-// Create an array of the values
-const valArr = Array.from(map1.values());
-console.log(valArr);
-
-// Create an array of the keys
-const keyArr = Array.from(map1.keys());
-console.log(keyArr);
+// VIf.CONVERT SET TO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
