c06f5bab70d61f82221065c0867cf5a21f2e1b34
valyndsilva <valyndsilva@gmail.com>
2021-06-10 06:59:07 +0100

    JavaScript Patterns

---
+++ JS_Sandbox/10.JavaScript_Patterns/app.js
@@ -0,0 +1,4 @@
+// What is a Pattern?
+//It's a re-usable solution that can be applied to occurring problems in software design (JS Applications)
+//Can also be thought of as programming templates
+//Situations vary significantly
