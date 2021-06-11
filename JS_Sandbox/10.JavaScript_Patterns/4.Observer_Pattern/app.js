9b6d9534e82fa5a7a89e83be7ef6834f0b888bfb
valyndsilva <valyndsilva@gmail.com>
2021-06-11 17:46:00 +0100

    JavaScript Patterns: Mediator Pattern using ES5

--- JS_Sandbox/10.JavaScript_Patterns/4.Observer_Pattern/app.js
+++ JS_Sandbox/10.JavaScript_Patterns/4.Observer_Pattern/app.js
@@ -14,7 +14,9 @@
       console.log(`You are now subscribed to ${fn.name}`);
     },
     unsubscribe: function(fn) {
-      // Filter out from the list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
+      // Filter out from the list whatever matches the callback function. 
+      // If there is no match, the callback gets to stay on the list. 
+      // The filter returns a new list and reassigns the list of observers.
       this.observers = this.observers.filter(function(item){
         if(item !== fn) {
           return item;
@@ -61,4 +63,3 @@
     console.log(`Current Seconds: ${new Date().getSeconds()}`);
   }
   
-  // Ex using ES6 Classes:
