a39a54f89aff6cc3b3222e588365e23dd85d246b
valyndsilva <valyndsilva@gmail.com>
2021-06-14 14:35:03 +0100

    Submit Posts and Show Alert

--- JS_Sandbox/12.CRUD_Project_using_webpack_babel/src/app.js
+++ JS_Sandbox/12.CRUD_Project_using_webpack_babel/src/app.js
@@ -10,8 +10,29 @@ import {ui} from "./ui";
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts(){
     http.get('http://localhost:3000/posts') // since get is an async method it returns a promise.
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err))
+}
+
+function submitPost(){
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+    const data = {
+        // title: title,
+        // body : body
+        title, // ES2015 syntax if key and value is same 
+        body
+    }
+
+    //Create post
+    http.post('http://localhost:3000/posts', data)
+    .then(data => {
+        getPosts();
+    })
     .catch(err => console.log(err))
 }
