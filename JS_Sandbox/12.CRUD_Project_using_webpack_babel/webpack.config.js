188125561519df09aeecd6024c20d8563a42f266
valyndsilva <valyndsilva@gmail.com>
2021-06-14 07:18:15 +0100

    CRUD Project using babel and webpack

--- JS_Sandbox/12.CRUD_Project_using_webpack_babel/webpack.config.js
+++ JS_Sandbox/12.CRUD_Project_using_webpack_babel/webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    app: ['@babel/polyfill', './src/app.js']
+    app: ['@babel/polyfill', './src/app.js'],
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
   module: {
     rules: [
